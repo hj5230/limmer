6feb17c90deaf56c62b0cf5aacd38ce3f30d89c2
hj5230 <hf305@foxmail.com>
2024-11-28 04:02:09 +0800

    feat(page): add page playground

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,9 @@ import { JSX } from 'preact/jsx-runtime'
 import { Page } from './typings'
 import { Chat } from './pages/Chat'
 import { Completion } from './pages/Completion'
+import { Playground } from './pages/Playground'
 
-import { Theme, Button } from '@radix-ui/themes'
+import { Theme, Flex, Button } from '@radix-ui/themes'
 
 function App() {
   const [page, setPage] = useState<Page>(Page.CHAT)
@@ -13,12 +14,17 @@ function App() {
   function framework(element: JSX.Element) {
     return (
       <Theme>
-        <Button onClick={() => setPage(Page.CHAT)}>
-          CHAT
-        </Button>
-        <Button onClick={() => setPage(Page.COMPLETION)}>
-          COMPLETION
-        </Button>
+        <Flex gap="2">
+          <Button onClick={() => setPage(Page.CHAT)}>
+            CHAT
+          </Button>
+          <Button onClick={() => setPage(Page.COMPLETION)}>
+            COMPLETION
+          </Button>
+          <Button onClick={() => setPage(Page.Playground)}>
+            PLAYGROUND
+          </Button>
+        </Flex>
         {element}
       </Theme>
     )
@@ -30,6 +36,9 @@ function App() {
 
     case Page.COMPLETION:
       return framework(<Completion />)
+
+    case Page.Playground:
+      return framework(<Playground />)
   }
 }
 
